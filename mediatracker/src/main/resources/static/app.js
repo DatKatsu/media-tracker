2bfaad094db8fb94b55f0c8a1f04cdaedccf942c
fookz <fookzhan.li@gmail.com>
2026-04-24 05:49:57 +0200

    impl focusout events to show/hide recommendations, wip click event to prefill form

--- mediatracker/src/main/resources/static/app.js
+++ mediatracker/src/main/resources/static/app.js
@@ -1,7 +1,7 @@
 import {addItem} from './mediaEntriesApi.js';
 import {fetchItems} from './mediaEntriesApi.js';
 import {clearFilter} from './mediaEntriesApi.js';
-import * as mediaSearchApi from './mediaSearchApi.js';
+import * as mediaSearchApi from "./mediaSearchApi.js";
 
 const DEBOUNCE_MS = 300;
 let debounceTimer = null;
@@ -13,6 +13,9 @@ document.getElementById('title-input').addEventListener('input', () =>
         debounceTimer = setTimeout(mediaSearchApi.search, DEBOUNCE_MS);
         
     });
+document.getElementById('title-input').addEventListener('focusout', mediaSearchApi.hideSearchResults);
+document.getElementById('title-input').addEventListener('focusin', mediaSearchApi.showSearchResults);
+
 document.getElementById('filter-type').addEventListener('change', fetchItems);
 document.getElementById('filter-status').addEventListener('change', fetchItems);
 
--- mediatracker/src/main/resources/static/mediaSearchApi.js
+++ mediatracker/src/main/resources/static/mediaSearchApi.js
@@ -4,18 +4,20 @@ const MIN_CHARS = 3;
 const MAX_RESULTS = 10;
 
 
-let currentResults = [];
+let currentSearchResults = [];
 
 const input = document.getElementById('title-input');
 const search_results = document.getElementById('search-results');
 const search_container = document.getElementById('search-container');
+const add_form = document.getElementById('add-form');
 
 export async function search() {
 
     const query = input.value;
-    if(query.length < MIN_CHARS)
+    if (query.length < MIN_CHARS) {
+        hideSearchResults();
         return;
-
+    }
     const params = new URLSearchParams();
     //const testQuery = 'One';
     params.append('query', query);
@@ -23,27 +25,79 @@ export async function search() {
     const response = await fetch(`${API_SEARCH_URL}?${params}`);
     const results = await response.json();
 
-    renderResults(results, query)
-    console.log(results); 
+
+    renderSearchResults(results, query)
+    console.log(results);
 }
 
-function renderResults(results, query) {
+function renderSearchResults(results, query) {
 
-    search_results.style.display = "flex";
-    
-    let listHTML = ``; 
-    results.forEach(result => {listHTML += listEntry(result)});
-    search_results.innerHTML = listHTML;
+    currentSearchResults = results;
+    const groupedResults = groupByMediaType(results);
+    console.log(groupedResults);
+
+    let columnHTML = ``;
+    Object.entries(groupedResults).forEach(([type, entries]) => {
+        columnHTML += `
+        <div class="search-column"> 
+            <h3 class="search-entry-header">${type}</h3>
+            <ul class="search-column-list">
+                ${entries.map(entry => listEntry(entry)).join('')}
+            </ul>
+        </div>
+        `
+    });
+
+
+    search_results.innerHTML = columnHTML;
+    document.querySelector(".search-column-list").addEventListener('mousedown', fillForm);
+
+    showSearchResults();
+}
+
+function fillForm(e)
+{
+    e.preventDefault();
+    console.log("Fill Form");
+    hideSearchResults();
 }
 
 function listEntry(entry) {
-    return `<li>
-    <div class="search-entry">
-        <img class="search-entry-image" src="${entry.imageUrl}" alt="${entry.title}">
-        <div class="search-entry-info">
-            <span class="search-entry-title">${entry.title}</span>
-            <span class="searh-entry-meta">Placeholder</span>
-        </div>
-    </div>
-    </li>`;
+    return `
+        <li class="search-entry">
+            <img class="search-entry-image" src="${entry.imageUrl}" alt="${entry.title}">
+            <div class="search-entry-info">
+                <span class="search-entry-title">${entry.title}</span>
+                <span class="searh-entry-meta">Placeholder</span>
+            </div>
+        </li>
+        `;
+}
+
+export function hideSearchResults()
+{
+    console.log("Hide Search Results");
+    search_results.style.display = "none";
+}
+
+export function showSearchResults()
+{
+    const query = input.value;
+    if (query.length < MIN_CHARS) {
+        return;
+    }
+
+    console.log("Show Search Results");
+    search_results.style.display = "flex";
+}
+
+function groupByMediaType(results) {
+    return results.reduce((acc, result) =>
+    {
+        const type = result.type;
+        if (!acc[type])
+            acc[type] = []
+        acc[type].push(result);
+        return acc;
+    }, {})
 }
